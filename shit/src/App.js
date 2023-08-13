fa5124f8b2aa76dcb1526125ce4c4d057cccab4e
hmk99 <mustaphadx1999@gmail.com>
2023-08-14 00:13:07 +0100

    shit2

--- shit/src/App.js
+++ shit/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>shit</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum repudiandae facere quos sed, quaerat dolorum iure ex veniam totam laboriosam aspernatur quia id, eius et at, praesentium distinctio quis a. Provident, a, aut necessitatibus, nisi placeat similique numquam sequi laboriosam hic veniam exercitationem. Illum, laudantium. Ex accusantium, repudiandae obcaecati molestiae eum ipsam, voluptas provident inventore id animi sint autem soluta itaque optio deserunt molestias enim. Tenetur dolore sit a ex in, nihil voluptates esse error tempora iste, facere velit nesciunt mollitia quasi itaque perspiciatis aut possimus quisquam illum, magnam quidem. Quo nostrum provident nisi neque earum repellendus nemo qui sit?</p>
     </div>
   );
 }
